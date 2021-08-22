4742e0df4ce74acecb53a6af8cc8fb797535be33
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-08-22 21:26:16 +0300

    Added jdbc to install

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+#Install Python Reqs
 pip3 install -r requirements.txt
 
-psql -d template1 -h localhost -p 5433 -U gpuser -f ./dwh/init_dwh.sql
+#Create DWH structure
+psql -d postgres -h localhost -p 5433 -U gpuser -f ./dwh/init_dwh.sql
 
+#copy JDBC driver
+mkdir ~/jdbc
+cp ./jdbc/postgresql-42.2.23.jar ~/jdbc/postgresql-42.2.23.jar
+
+#Install Airflow DAGs
+mkdir ~/airflow/dags
 ./update_dags.sh
